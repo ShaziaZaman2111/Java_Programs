f70a8233029daa5bbcd585e8215e53e4571e4abf
Shazia Zaman <shazia.zaman2111@gmail.com>
2021-08-23 18:10:32 +0600

    Update SeriesPrinter.java

--- SeriesPrinter.java
+++ SeriesPrinter.java
@@ -1,3 +1,15 @@
+/*
+Create a class SeriesPrinter. 
+ Add some overloaded methods as follows:
+o printSeries(int n): prints all the integers up to n from 0.
+ Example: printSeries(5) -> 0 1 2 3 4 5
+o printSeries(int startIndex, int endIndex): prints all integers from startIndex to endIndex.
+ Example: printSeries(5, 10) -> 5 6 7 8 9 10
+o printSeries(int startIndex, int endIndex, int interval): prints all the integers form startIndext
+with a given interval.
+ Example: printSeries(5, 15, 3) -> 5 8 11 14
+ include a main method. In the main method test all the overloaded methods of SeriesPrinter
+*/
 public class SeriesPrinter {
     	int i, n, startIndex, endIndex, interval;
 	SeriesPrinter(){
